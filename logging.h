f2373a5964eeac0f787c8230556e5a0d3c093cc9
Honcharov Andrii <AHoncharov@luxoft.com>
2018-08-16 09:42:22 +0300

    Change logging ti file time format

--- logging.h
+++ logging.h
@@ -2,26 +2,10 @@
 #define LOGGING_H
 
 #include <chrono>
-#include "string"
+#include <string>
 #include <ctime>
-#include <stdio.h>
-
-std::string& ltrim(std::string& str, const std::string& chars = "\t\n\v\f\r ")
-{
-    str.erase(0, str.find_first_not_of(chars));
-    return str;
-}
-
-std::string& rtrim(std::string& str, const std::string& chars = "\t\n\v\f\r ")
-{
-    str.erase(str.find_last_not_of(chars) + 1);
-    return str;
-}
-
-std::string& trim(std::string& str, const std::string& chars = "\t\n\v\f\r ")
-{
-    return ltrim(rtrim(str, chars), chars);
-}
+#include <iomanip>
+#include <sstream>
 
 class LoggerSingleton
 {
@@ -36,22 +20,18 @@ public:
     template<typename... Args>
     void writeToFile(std::string str, Args... args)
     {
-        std::chrono::time_point<std::chrono::system_clock> p = std::chrono::system_clock::now();
+        std::time_t t = std::time(nullptr);
+        std::tm tm = *std::localtime(&t);
 
-        std::time_t today_time = std::chrono::system_clock::to_time_t(p);
-        std::string strTimeDate = std::ctime(&today_time);
+        std::stringstream buffer;
 
-        //pFile = fopen(fileName.c_str(),"w+");
+        buffer << std::put_time(&tm, "%H:%M:%S %Y");
 
-        //fseek(pFile, 0, SEEK_END);
-
-        strTimeDate = trim(strTimeDate);
+        std::string strTimeDate = buffer.str();
 
         std::string strWriteToFile = strTimeDate + " " + str + "\n";
 
         fprintf (pFile, strWriteToFile.c_str(), args...);
-
-        //fclose (pFile);
     }
 private:
 
@@ -69,8 +49,6 @@ private:
         fileName = "./" + strMinutes + ".txt";
 
         pFile = fopen(fileName.c_str(),"w");
-
-        //fclose (pFile);
     }
     LoggerSingleton(LoggerSingleton const &);
     LoggerSingleton & operator=(LoggerSingleton const &);
@@ -79,8 +57,6 @@ private:
         fclose (pFile);
     }
 };
-
-//qDebug(str, ##__VA_ARGS__);
 
 #ifndef NDEBUG
 
